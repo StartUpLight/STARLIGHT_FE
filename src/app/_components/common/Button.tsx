980f51753865c0243af18ce4bcfe35726f6b4124
chasyuss <cotn963@gmail.com>
2025-10-18 18:05:58 +0900

    feat: (SRLT-18) 버튼 크기 및 색상 추가

--- src/app/_components/common/Button.tsx
+++ src/app/_components/common/Button.tsx
@@ -2,6 +2,8 @@ import React from "react";
 
 interface ButtonProps {
   text: string;
+  size?: "S" | "M" | "L";
+  color?: "primary" | "secondary";
   rounded?: string;
   onClick?: () => void;
   className?: string;
@@ -9,14 +11,39 @@ interface ButtonProps {
 
 function Button({
   text,
-  rounded = "rounded-full",
+  size = "M",
+  color = "primary",
+  rounded = "",
   onClick,
   className = "",
 }: ButtonProps) {
+  const sizeClasses = {
+    S: "ds-caption px-3 py-[6px]",
+    M: "ds-text px-4 py-[8px]",
+    L: "ds-text px-5 py-[10px]",
+  };
+
+  const variantClasses = {
+    primary:
+      "bg-primary-500 text-white hover:bg-primary-600 active:bg-primary-700",
+    secondary:
+      "bg-white text-gray-900 border border-gray-300 hover:bg-gray-100 active:bg-gray-200",
+  };
+
   return (
     <button
       onClick={onClick}
-      className={`w-full py-2 bg-primary-500 text-white font-medium ${rounded} hover:bg-primary-700 cursor-pointer ${className} `}
+      className={`
+        w-full 
+        font-medium 
+        transition 
+        cursor-pointer 
+        rounded-[8px]
+        ${sizeClasses[size]} 
+        ${variantClasses[color]} 
+        ${rounded} 
+        ${className}
+      `}
     >
       {text}
     </button>
