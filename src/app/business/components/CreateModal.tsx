9648a9d08f2786e37aa236c3372cd7db49f16da7
chasyuss <cotn963@gmail.com>
2025-10-18 19:58:11 +0900

    feat: (SRLT-18) 모달 close 버튼 추가

--- src/app/business/components/CreateModal.tsx
+++ src/app/business/components/CreateModal.tsx
@@ -1,15 +1,25 @@
 import Button from "@/app/_components/common/Button";
 import React from "react";
+import Image from "next/image";
 
 interface CreateModalProps {
   title: string;
   subtitle: string;
+  onClose?: () => void;
 }
 
-const CreateModal = ({ title, subtitle }: CreateModalProps) => {
+const CreateModal = ({ title, subtitle, onClose }: CreateModalProps) => {
   return (
     <div className="fixed inset-0 z-20 flex items-center justify-center bg-black/20">
-      <div className="flex p-10 w-[585px] flex-col items-start rounded-[20px] bg-gray-100">
+      <div className="relative flex p-10 w-[585px] flex-col items-start rounded-[20px] bg-gray-100">
+        <button
+          onClick={onClose}
+          className="absolute top-7 right-7 cursor-pointer"
+          aria-label="닫기"
+        >
+          <Image src="/icons/close.svg" alt="닫기" width={24} height={24} />
+        </button>
+
         <div className="text-gray-800 ds-heading font-bold">{title}</div>
 
         <div className="mt-2 text-gray-600 ds-text font-medium whitespace-pre-line">
@@ -17,7 +27,8 @@ const CreateModal = ({ title, subtitle }: CreateModalProps) => {
         </div>
 
         <div className="w-[505px] h-[144px] bg-white mt-8" />
-        <Button text="생성하기" className="mt-8" />
+
+        <Button text="생성하기" className="mt-8 rounded-full" />
       </div>
     </div>
   );
