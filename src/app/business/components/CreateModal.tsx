0636193c305c5b026d3aa9c717c2899f9bef8652
chasyuss <cotn963@gmail.com>
2025-10-20 16:30:40 +0900

    feat: (SRLT-22) 채점하기 모달 연결 후 리포트 페이지로 이동하도록 연결

--- src/app/business/components/CreateModal.tsx
+++ src/app/business/components/CreateModal.tsx
@@ -1,17 +1,26 @@
-import Button from "@/app/_components/common/Button";
-import React from "react";
-import Close from "@/assets/icons/close.svg";
+import Button from '@/app/_components/common/Button';
+import React from 'react';
+import Close from '@/assets/icons/close.svg';
+import { createPortal } from 'react-dom';
 
 interface CreateModalProps {
   title: string;
   subtitle: string;
+  buttonText?: string;
   onClose?: () => void;
+  onClick?: () => void;
 }
 
-const CreateModal = ({ title, subtitle, onClose }: CreateModalProps) => {
-  return (
-    <div className="fixed inset-0 z-20 flex items-center justify-center bg-black/20">
-      <div className="relative flex p-10 w-[585px] flex-col items-start rounded-[20px] bg-gray-100">
+const CreateModal = ({
+  title,
+  subtitle,
+  buttonText = '생성하기',
+  onClose,
+  onClick,
+}: CreateModalProps) => {
+  return createPortal(
+    <div className="fixed inset-0 z-[9999] flex items-center justify-center bg-black/20">
+      <div className="relative flex w-[585px] flex-col items-start rounded-[20px] bg-gray-100 p-10">
         <button
           onClick={onClose}
           className="absolute top-7 right-7 cursor-pointer"
@@ -20,17 +29,22 @@ const CreateModal = ({ title, subtitle, onClose }: CreateModalProps) => {
           <Close />
         </button>
 
-        <div className="text-gray-800 ds-heading font-bold">{title}</div>
+        <div className="ds-heading font-bold text-gray-800">{title}</div>
 
-        <div className="mt-2 text-gray-600 ds-text font-medium whitespace-pre-line">
+        <div className="ds-subtext mt-2 font-medium whitespace-pre-line text-gray-600">
           {subtitle}
         </div>
 
-        <div className="w-[505px] h-[144px] bg-white mt-8" />
+        <div className="mt-8 h-[144px] w-[505px] bg-white" />
 
-        <Button text="생성하기" className="mt-8 rounded-full" />
+        <Button
+          text={buttonText}
+          className="mt-8 rounded-full"
+          onClick={onClick}
+        />
       </div>
-    </div>
+    </div>,
+    document.body
   );
 };
 
