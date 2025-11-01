eb14e3d4b8d9a392f56da95c311216294b26bcd0
chasyuss <cotn963@gmail.com>
2025-11-01 20:25:06 +0900

    feat: (SRLT-36) maze 스니펫 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import localFont from 'next/font/local';
 import './globals.css';
 import Header from './_components/common/Header';
 import Providers from './providers';
+import Script from 'next/script';
 
 const pretendard = localFont({
   src: '../fonts/PretendardVariable.ttf',
@@ -21,6 +22,22 @@ export default function RootLayout({
           <main className="pt-[60px]">{children}</main>
         </Providers>
       </body>
+
+      <Script id="maze-inline" strategy="afterInteractive">
+        {`(function (m, a, z, e) {
+    var s, t;
+    try { t = m.sessionStorage.getItem('maze-us'); } catch (err) {}
+    if (!t) {
+      t = new Date().getTime();
+      try { m.sessionStorage.setItem('maze-us', t); } catch (err) {}
+    }
+    s = a.createElement('script');
+    s.src = z + '?apiKey=' + e;
+    s.async = true;
+    a.getElementsByTagName('head')[0].appendChild(s);
+    m.mazeUniversalSnippetApiKey = e;
+  })(window, document, 'https://snippet.maze.co/maze-universal-loader.js', 'fb7a611e-c8b2-4f26-bbde-7586411b578e');`}
+      </Script>
     </html>
   );
 }
