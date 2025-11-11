45aab6365f35ae9093eab2efb0827aa72d5b17d0
chasyuss <cotn963@gmail.com>
2025-11-11 22:46:10 +0900

    feat: (SRLT-38) 체크리스트 연동

--- src/types/business/checklist.type.ts
+++ src/types/business/checklist.type.ts
@@ -12,3 +12,21 @@ export type SectionItem = {
   checklist?: ChecklistProps[];
 };
 export type Section = { title: string; items: SectionItem[] };
+
+///
+type Content =
+  | { type: 'image'; src: string; caption?: string }
+  | { type: 'table'; columns: string[]; rows: string[][] }
+  | { type: 'text'; value: string };
+
+type Block = {
+  meta: { title: string };
+  content: Content[];
+};
+
+export type CheckListResponse = {
+  subSectionType: string;
+  checks: boolean[];
+  meta: { author: string; createdAt: string };
+  blocks: Block[];
+};
