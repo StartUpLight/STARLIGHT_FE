6e727f2dd21d2b8a5a06a39218f54da745a64bb0
chasyuss <cotn963@gmail.com>
2025-11-15 17:55:50 +0900

    fix: (SRLT-78) createModal 사진 변경

--- src/app/business/components/CreateModal.tsx
+++ src/app/business/components/CreateModal.tsx
@@ -1,9 +1,10 @@
 'use client';
-import Button from '@/app/_components/common/Button';
+
 import React from 'react';
-import Close from '@/assets/icons/close.svg';
 import { createPortal } from 'react-dom';
 import Image from 'next/image';
+import Button from '@/app/_components/common/Button';
+import Close from '@/assets/icons/close.svg';
 
 interface CreateModalProps {
   title: string;
@@ -11,6 +12,10 @@ interface CreateModalProps {
   buttonText?: string;
   onClose?: () => void;
   onClick?: () => void;
+  imageSrc?: string;
+  imageAlt?: string;
+  imageWidth?: number;
+  imageHeight?: number;
 }
 
 const CreateModal = ({
@@ -19,10 +24,14 @@ const CreateModal = ({
   buttonText = '생성하기',
   onClose,
   onClick,
+  imageSrc = '/images/bussinessModal_Image.png',
+  imageAlt = '사진',
+  imageWidth = 273,
+  imageHeight = 190,
 }: CreateModalProps) => {
   return createPortal(
     <div className="fixed inset-0 z-[200] flex items-center justify-center bg-black/20">
-      <div className="relative flex w-[585px] flex-col items-start rounded-[20px] bg-gray-100 p-10">
+      <div className="relative flex w-[585px] flex-col items-center justify-center rounded-[20px] bg-gray-100 p-10">
         <button
           onClick={onClose}
           className="absolute top-7 right-7 cursor-pointer"
@@ -31,22 +40,18 @@ const CreateModal = ({
           <Close />
         </button>
 
-        <div className="ds-heading font-bold text-gray-800">{title}</div>
+        <Image
+          src={imageSrc}
+          alt={imageAlt}
+          width={imageWidth}
+          height={imageHeight}
+          className="self-center object-contain"
+        />
 
-        <div className="ds-subtext mt-2 font-medium whitespace-pre-line text-gray-600">
+        <div className="ds-heading mt-8 font-bold text-gray-800">{title}</div>
+
+        <div className="ds-subtext mt-2 text-center font-medium whitespace-pre-line text-gray-600">
           {subtitle}
-        </div>
-
-        {/* <div className="mt-8 h-[144px] w-[505px] bg-white" /> */}
-
-        <div className="mt-8 flex h-[144px] w-[505px] items-center justify-center">
-          <Image
-            src="/images/spinner.gif"
-            alt="미리보기 호버 말풍선"
-            width={100}
-            height={100}
-            className="self-center object-contain"
-          />
         </div>
 
         <Button
