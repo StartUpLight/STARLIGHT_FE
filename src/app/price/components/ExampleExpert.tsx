959ab706e7bc5f9d912398ee20f210126930c582
chasyuss <cotn963@gmail.com>
2025-11-17 23:35:41 +0900

    fix: (SRLT-83) pricingCard width 설정 삭제

--- src/app/price/components/ExampleExpert.tsx
+++ src/app/price/components/ExampleExpert.tsx
@@ -4,7 +4,7 @@ import ArrowRight from '@/assets/icons/right_icon.svg';
 
 const ExampleExpert = () => {
   return (
-    <div className="bg-gray-80 mt-8 flex w-full flex-row items-center justify-between rounded-[12px] p-6">
+    <div className="bg-gray-80 mt-8 flex w-full flex-row items-center justify-between rounded-xl p-6">
       <div className="ds-subtitle font-semibold text-gray-900">
         {' '}
         전문가 리포트 예시가 필요하신가요?
@@ -15,7 +15,7 @@ const ExampleExpert = () => {
         size="L"
         color="bg-gray-200"
         icon={<ArrowRight />}
-        className="h-11 gap-1 rounded-[8px] hover:bg-gray-300 active:bg-gray-300"
+        className="h-11 gap-1 rounded-lg hover:bg-gray-300 active:bg-gray-300"
       />
     </div>
   );
--- src/app/price/page.tsx
+++ src/app/price/page.tsx
@@ -6,7 +6,7 @@ import ExampleExpert from './components/ExampleExpert';
 const page = () => {
   return (
     <div className="flex w-full flex-col items-start justify-center px-[132px] pt-14 pb-[169px]">
-      <div className="text-[36px] leading-[1.5] font-bold text-gray-900">
+      <div className="text-[36px] leading-1.5 font-bold text-gray-900">
         스타라이트 <span className="gradation2">3가지</span> 요금제
       </div>
 
