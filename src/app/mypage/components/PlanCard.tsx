fab6962245d44ad5815c4aeff25be68188b69664
parknari02 <pnr021118@naver.com>
2025-11-19 03:08:40 +0900

    fix: (SRLT-88) disable 조건 변경

--- src/app/mypage/components/PlanCard.tsx
+++ src/app/mypage/components/PlanCard.tsx
@@ -108,12 +108,13 @@ export default function PlanCard({
                         <ArrowRightIcon />
                     </button>
                     <button
-                        disabled={!isAiReportEnabled}
+                        disabled={!isExpertReportEnabled}
                         onClick={handleNewExpertClick}
                         type="button"
-                        className={`ds-caption font-medium whitespace-nowrap flex items-center justify-center rounded-[8px] transition px-3 py-2 ${isAiReportEnabled
-                            ? 'bg-white text-gray-900 border border-gray-300 hover:bg-gray-100 active:bg-gray-200 cursor-pointer'
-                            : 'bg-gray-200 text-gray-500 border border-gray-200 cursor-not-allowed opacity-50'
+                        className={`ds-caption font-medium whitespace-nowrap flex items-center justify-center rounded-[8px] transition px-3 py-2 
+                            ${isExpertReportEnabled
+                                ? 'bg-white text-gray-900 border border-gray-300 hover:bg-gray-100 active:bg-gray-200 cursor-pointer'
+                                : 'bg-gray-200 text-gray-500 border border-gray-200 cursor-not-allowed opacity-50'
                             }`}
                     >
                         새로운 전문가 연결
