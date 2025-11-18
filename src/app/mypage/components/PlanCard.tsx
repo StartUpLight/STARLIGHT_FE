9e04ef84f011c3160e89fb2e65e9a579356d90d2
parknari02 <pnr021118@naver.com>
2025-11-19 00:19:46 +0900

    fix: (SRLT-88) 마이페이지 버튼 활성화 수정

--- src/app/mypage/components/PlanCard.tsx
+++ src/app/mypage/components/PlanCard.tsx
@@ -37,6 +37,10 @@ export default function PlanCard({
         setPlanId(businessPlanId);
         router.push(`/report`);
     };
+    const handleNewExpertClick = () => {
+        setPlanId(businessPlanId);
+        router.push(`/expert`);
+    };
 
     return (
         <div className="w-full rounded-[12px] bg-white px-6 pt-6 pb-4 space-y-6">
@@ -104,8 +108,13 @@ export default function PlanCard({
                         <ArrowRightIcon />
                     </button>
                     <button
+                        disabled={!isAiReportEnabled}
+                        onClick={handleNewExpertClick}
                         type="button"
-                        className="h-[28px] border-gray-300 border-[1px] text-gray-900 cursor-pointer whitespace-nowrap flex items-center justify-center rounded-[4px] transition ds-caption font-medium p-2"
+                        className={`ds-caption font-medium whitespace-nowrap flex items-center justify-center rounded-[8px] transition px-3 py-2 ${isAiReportEnabled
+                            ? 'bg-white text-gray-900 border border-gray-300 hover:bg-gray-100 active:bg-gray-200 cursor-pointer'
+                            : 'bg-gray-200 text-gray-500 border border-gray-200 cursor-not-allowed opacity-50'
+                            }`}
                     >
                         새로운 전문가 연결
                     </button>
