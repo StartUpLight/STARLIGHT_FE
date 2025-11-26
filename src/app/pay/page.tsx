58209ed949231126df1953519799718cbc37441a
chasyuss <cotn963@gmail.com>
2025-11-26 18:01:17 +0900

    feat: (SRLT-106) 전문가 결제 ui 변경

--- src/app/pay/page.tsx
+++ src/app/pay/page.tsx
@@ -1,36 +1,26 @@
 'use client';
-import { useState } from "react";
-import Button from "../_components/common/Button";
-import { useRouter } from "next/navigation";
-import OrderDetails from "./components/OrderDetails";
-import PaymentMethod from "./components/PaymentMethod";
-import PaymentAmount from "./components/PaymentAmount";
+import Button from '../_components/common/Button';
+import { useRouter } from 'next/navigation';
+import OrderDetails from './components/OrderDetails';
+import PaymentAmount from './components/PaymentAmount';
 
 const Page = () => {
-    const router = useRouter();
-    const [quantity, setQuantity] = useState(1);
-    const [selectedPayment, setSelectedPayment] = useState('card');
-    return (
-        <div className="min-h-screen bg-white">
-            <div className="mx-auto py-[30px] min-w-[550px] max-w-[944px] px-6">
-                <h1 className="ds-title font-semibold text-gray-900 mb-6">결제</h1>
-                <OrderDetails
-                    quantity={quantity}
-                    onQuantityChange={setQuantity}
-                />
-                <PaymentMethod
-                    selectedPayment={selectedPayment}
-                    onPaymentChange={setSelectedPayment}
-                />
-                <PaymentAmount quantity={quantity} />
-                <Button
-                    text="결제하기"
-                    className="w-full h-[44px]"
-                    onClick={() => router.push('/pay/complete')}
-                />
-            </div>
-        </div>
-    )
-}
+  const router = useRouter();
 
-export default Page
+  return (
+    <div className="h-full bg-white">
+      <div className="mx-auto max-w-[944px] min-w-[550px] px-6 py-[30px]">
+        <h1 className="ds-title mb-6 font-semibold text-gray-900">결제</h1>
+        <OrderDetails />
+        <PaymentAmount />
+        <Button
+          text="결제하기"
+          className="h-11 w-full"
+          onClick={() => router.push('/pay/complete')}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Page;
